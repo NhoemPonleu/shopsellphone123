15eb13cfaba8535ac2dd3c31ee1be30ee4bd8109
ponleunhoem@gmail.com <ponleunhoem@gmail.com>
2022-10-31 14:27:07 +0700

    'brand'

---
+++ ShopPhone-111/src/test/java/com/example/demo/ShopPhone111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopPhone111ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
